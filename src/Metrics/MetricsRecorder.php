5bea4d9467d47b6dfc74b471350f7d1040a56dff
Michał Mleczko <do@mleczki.pl>
2026-09-09 07:12:35 +0200

    feat(metrics): support counters

--- src/Metrics/MetricsRecorder.php
+++ src/Metrics/MetricsRecorder.php
@@ -11,6 +11,13 @@ use function Sentry\trace_metrics;
 final readonly class MetricsRecorder implements MetricsRecorderInterface
 {
     #[\Override]
+    public function count(string $name, int|float $value, array $attributes = []): void
+    {
+        trace_metrics()->count($name, $value, $attributes);
+        trace_metrics()->flush();
+    }
+
+    #[\Override]
     public function distribution(string $name, int|float $value, array $attributes = [], ?Unit $unit = null): void
     {
         trace_metrics()->distribution($name, $value, $attributes, $unit);
--- src/Metrics/MetricsRecorderInterface.php
+++ src/Metrics/MetricsRecorderInterface.php
@@ -9,5 +9,8 @@ use Sentry\Unit;
 interface MetricsRecorderInterface
 {
     /** @param array<string, int|float|string|bool> $attributes */
+    public function count(string $name, int|float $value, array $attributes = []): void;
+
+    /** @param array<string, int|float|string|bool> $attributes */
     public function distribution(string $name, int|float $value, array $attributes = [], ?Unit $unit = null): void;
 }
